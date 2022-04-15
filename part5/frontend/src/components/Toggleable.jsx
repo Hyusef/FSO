4f003312486f79f5624dd468a6d88a4aeb006274
therealestdeal <mieyle@protonmail.com>
2022-04-15 12:54:22 +0100

    name change

---
+++ part5/frontend/src/components/Toggleable.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import propTypes from 'prop-types'
+
+
+function Toggleable(props) {
+  const [visible, setVisible] = useState(false);
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const ShowWhenVisible = { display: visible ? "" : "none" };
+
+
+  
+  const toggleVisib = () => {
+    
+    setVisible(!visible);
+  };
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisib}>{props.buttonLabel}</button>
+      </div>
+
+      <div style={ShowWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisib}>Cancel</button>
+      </div>
+    </div>
+  );
+}
+
+Toggleable.propTypes={
+  buttonLabel: propTypes.string.isRequired
+}
+
+export default Toggleable;
+

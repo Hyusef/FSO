9c629239aef813ba8c670dfabb4493911104663a
therealestdeal <mieyle@protonmail.com>
2022-05-09 10:06:53 +0100

    Add functionality to filter books by genre

--- part8/authorsview/src/components/Books.jsx
+++ part8/authorsview/src/components/Books.jsx
@@ -1,33 +1,65 @@
-const Books = (props) => {
-    if (!props.show) {
-      return null
-    }
+import { useState, useEffect } from "react";
 
-/*     console.log() */;
-    const books =props.myDataBooks.data.allBooks
-  
-    return (
-      <div>
-        <h2>books</h2>
-  
-        <table>
-          <tbody>
-            <tr>
-              <th></th>
-              <th>author</th>
-              <th>published</th>
-            </tr>
-            {books.map((a) => (
-              <tr key={a.title}>
-                <td>{a.title}</td>
-                <td>{a.author}</td>
-                <td>{a.published}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
+const Books = (props) => {
+  const [showBooks, setShowBooks] = useState([]);
+
+  /*     console.log() */
+  let books;
+  books = props.myDataBooks.data.allBooks;
+  useEffect(() => {
+    console.log(books);
+    setShowBooks(books);
+  }, []);
+
+  const genresHandler = (genre) => {
+    const genreFiltered = books.filter((ele) => {
+      return ele.genres.includes(genre);
+    });
+    setShowBooks(genreFiltered);
+  };
+
+  let myArr = [];
+  if (!props.show) {
+    return null;
   }
-  
-  export default Books
+  return (
+    <div>
+      <h2>books</h2>
+      {books &&
+        books.map((a) =>
+          a.genres.forEach((b) => {
+            myArr.push(b);
+          })
+        )}
+
+      {myArr
+        .filter((v, i, a) => a.indexOf(v) == i)
+        .map((e, i) => (
+          <button key={e + i} onClick={() => genresHandler(e)}>
+            {e}
+          </button>
+        ))}
+      <button onClick={() => setShowBooks()}> All Genres</button>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {showBooks.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author.name}</td>
+              <td>{a.published}</td>
+              <td>{a.genres[0]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Books;

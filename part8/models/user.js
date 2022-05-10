c8947b955573c9614a98eb9db625f25c25d7b977
therealestdeal <mieyle@protonmail.com>
2022-05-10 19:05:55 +0100

    Add fav genre field to user schema

--- part8/models/user.js
+++ part8/models/user.js
@@ -1,17 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: 3,
   },
   friends: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Person'
-    }
+      ref: "Person",
+    },
   ],
-})
+  favoriteGenre: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+});
 
-module.exports = mongoose.model('User', schema)
+module.exports = mongoose.model("User", schema);

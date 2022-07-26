26d1fc034cef5d9be650d83dd946e27e1f063c73
hyusef <mieyle@protonmail.com>
2022-07-26 17:04:07 +0100

    Add update and get single todo operations

--- part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,72 @@
-const express = require('express');
-const { Todo } = require('../mongo')
+const express = require("express");
+const { Todo } = require("../mongo");
 const router = express.Router();
-
 /* GET todos listing. */
-router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+router.get("/", async (_, res) => {
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
   res.send(todo);
 });
 
-const singleRouter = express.Router();
+const singleRouter = express.Router({ mergeParams: true });
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  const { id } = req.params;
+  try {
+    req.todo = await Todo.findById(id);
+  } catch {
+    if (!req.todo) return res.sendStatus(404);
+    console.log("error");
+  }
 
-  next()
-}
+  next();
+};
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+singleRouter.delete("/", async (req, res) => {
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.get("/", async (req, res) => {
+  try {
+    res.json(req.todo);
+  } catch {
+    if (!todo) return res.sendStatus(404);
+    console.log("error");
+  }
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.put("/", async (req, res) => {
+  console.log(req.todo.text, req.body.text);
+  try {
+    await Todo.findOneAndUpdate(
+      req.todo.text,
+      req.body,
+      { new: true },
+      function (err, task) {
+        if (err) res.send(err);
+        res.json(task);
+      }
+    );
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
-
+router.use("/:id", findByIdMiddleware, singleRouter);
 
 module.exports = router;
